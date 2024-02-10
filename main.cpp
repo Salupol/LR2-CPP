b857ab93471796eb0f057e01160e870f9b11b061
Pavlo <44544644+Salupol@users.noreply.github.com>
2024-02-10 16:44:07 +0200

    2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
-#include "cmath"
+#include <cmath>
+#include "string"
 int main() {
     using namespace std;
     while (true) {
         int n1;
         int n2;
-        cout << "Enter a number or -1 to exit: ";
+        string answer;
+        cout << "Enter a number or: ";
         cin >> n1;
-        if (n1 == -1) {
-            break;
-        }
         cout << "Enter another number: ";
         cin >> n2;
         double pr = 1.0;
         for (int i = n1; i <= n2; i++) {
             pr *= 1 - (pow(-1, i - 1) / (3 * i - 1));
             cout << pr << endl;
+        }
+        cout << "The product is: " << pr << endl;
+        cout << "Do you want to continue? (y/n): ";
+        cin >> answer;
+        if (answer == "n") {
+            break;
         }
     }
 }
