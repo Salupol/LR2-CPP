c142794847c162feba38ffabdb0e679f89540f3a
Pavlo <44544644+Salupol@users.noreply.github.com>
2024-03-04 10:21:40 +0200

    3

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <cmath>
 #include "string"
+
+using namespace std;
+
 int main() {
-    using namespace std;
-    while (true) {
+    string answer;
+    do{
         int n1;
         int n2;
-        string answer;
-        cout << "Enter a number or: ";
+        cout << "Enter a number: ";
         cin >> n1;
         cout << "Enter another number: ";
         cin >> n2;
+        if (n1 > n2) {
+            cout << "The first number must be less than the second number." << endl;
+            continue;
+        }
         double pr = 1.0;
         for (int i = n1; i <= n2; i++) {
             pr *= 1 - (pow(-1, i - 1) / (3 * i - 1));
@@ -19,8 +25,6 @@ int main() {
         cout << "The product is: " << pr << endl;
         cout << "Do you want to continue? (y/n): ";
         cin >> answer;
-        if (answer == "n") {
-            break;
-        }
+
+        } while (answer == "y");
     }
-}
